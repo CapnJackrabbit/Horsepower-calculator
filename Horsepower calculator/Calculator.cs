df84c7f67b3843bc6e0afb0238011c6e8fa024bc
CapnJackrabbit <acemustang@live.com>
2023-10-03 13:27:34 -0300

    Primeira versão

---
+++ Horsepower calculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Horsepower_calculator
+{
+    internal class Calculator
+    {
+        public int Constant = 5252;
+        public double Torque {  get; set; }
+        public double Rpm { get; set; }
+        public double Horsepower { get; private set; }
+
+
+        public Calculator(double torque, double rpm)
+        {
+            this.Torque = torque * 7.233;
+            this.Rpm = rpm;
+        }
+
+        public double Calculate()
+        {
+            Horsepower = (Torque * Rpm) / Constant;
+            return Horsepower;
+        }
+
+        public override string ToString()
+        {
+            return "Potência máxima (HP): " + Calculate().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Horsepower calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Horsepower_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o torque (kgfm): ");
+            double torque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Entre com o RPM máximo: ");
+            int rpm = int.Parse(Console.ReadLine());
+
+            Calculator C1 = new Calculator(torque, rpm);
+
+            Console.WriteLine(C1 + "\n");
+        }
+    }
+}
